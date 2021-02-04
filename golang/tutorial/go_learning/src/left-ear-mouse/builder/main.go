c2902b6d646139316ee201221190da166cbe3e88
guanzhongcai <zhongcai.guan@united-imaging.com>
2021-02-04 22:25:26 +0800

    go 编程模式 builder

---
+++ golang/tutorial/go_learning/src/left-ear-mouse/builder/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"time"
+)
+
+type Config struct {
+	Protocol string
+	Timeout  time.Duration
+	MaxConns int
+	TLS      *tls.Config
+}
+
+type Server struct {
+	Addr string
+	Port int
+	Conf *Config
+}
+
+//使用一个builder类来做包装
+type ServerBuilder struct {
+	Server
+}
+
+func (sb *ServerBuilder) Create(addr string, port int) *ServerBuilder {
+	sb.Server.Addr = addr
+	sb.Server.Port = port
+	//其它代码设置其它成员的默认值
+	return sb
+}
+
+func (sb *ServerBuilder) WithProtocol(protocol string) *ServerBuilder {
+	sb.Server.Conf.Protocol = protocol
+	return sb
+}
+
+func (sb *ServerBuilder) WithMaxConn(maxconn int) *ServerBuilder {
+	sb.Server.Conf.MaxConns = maxconn
+	return sb
+}
+
+func (sb *ServerBuilder) WithTimeOut(timeout time.Duration) *ServerBuilder {
+	sb.Server.Conf.Timeout = timeout
+	return sb
+}
+
+func (sb *ServerBuilder) WithTLS(tls *tls.Config) *ServerBuilder {
+	sb.Server.Conf.TLS = tls
+	return sb
+}
+
+func (sb *ServerBuilder) Build() Server {
+	return sb.Server
+}
+
+func main() {
+
+	sb := ServerBuilder{}
+	server := sb.Create("127.0.0.1", 8080).
+		WithProtocol("udp").
+		WithMaxConn(1024).
+		WithTimeOut(30 * time.Second).
+		Build()
+}
