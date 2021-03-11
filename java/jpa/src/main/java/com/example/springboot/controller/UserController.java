e73af6b6c00b378736e91acf9c35faa5bc4c4f87
guanzhongcai <zhongcai.guan@united-imaging.com>
2021-03-12 07:12:51 +0800

    logging & logback practice

--- java/jpa/src/main/java/com/example/springboot/controller/UserController.java
+++ java/jpa/src/main/java/com/example/springboot/controller/UserController.java
@@ -2,18 +2,25 @@ package com.example.springboot.controller;
 
 import com.example.springboot.entity.User;
 import com.example.springboot.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     @Autowired
     UserRepository userRepository;
 
     @GetMapping("/user/{id}")
     public User getUser(@PathVariable("id") Integer id) {
+        log.info("this is a info msg: {}", new Date());
+
         User user = new User();//userRepository.findById(id);
         return user;
     }
