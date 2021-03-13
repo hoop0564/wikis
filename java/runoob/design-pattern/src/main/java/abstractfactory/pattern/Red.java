6350580db3161d97c90c6e5594b16f8bf4217ed6
guanzhongcai <zhongcai.guan@united-imaging.com>
2021-03-14 07:13:14 +0800

    factory & abstract factory pattern

---
+++ java/runoob/design-pattern/src/main/java/abstractfactory/pattern/Red.java
@@ -0,0 +1,9 @@
+package abstractfactory.pattern;
+
+public class Red implements Color {
+
+    @Override
+    public void fill() {
+        System.out.println("Inside Red::fill() method.");
+    }
+}
