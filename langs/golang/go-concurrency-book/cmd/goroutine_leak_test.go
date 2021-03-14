1074539ca7912e6b57045bb7a98bd9cde6d27278
guanzhongcai <zhongcai.guan@united-imaging.com>
2021-03-14 11:12:33 +0800

    tidy directory

---
+++ langs/golang/go-concurrency-book/cmd/goroutine_leak_test.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+
+func TestPreventGoroutineLeak(t *testing.T) {
+	doWork := func(
+		done <-chan interface{},
+		strings <-chan string,
+	) <-chan interface{} {
+
+		terminated := make(chan interface{})
+		go func() {
+			defer fmt.Println("doWork exited.")
+			defer close(terminated)
+			for {
+				select {
+				case s := <-strings:
+					// do sth
+					fmt.Println(s)
+				case <-done:
+					return
+				default:
+					//fmt.Println("waiting..")
+				}
+			}
+		}()
+		return terminated
+	}
+
+	done := make(chan interface{})
+	s := make(chan string,1)
+	s <- "nil"
+	terminated := doWork(done, nil)
+
+	go func() {
+		// cancel op after 1 second
+		time.Sleep(1 * time.Second)
+		fmt.Println("canceling doWork goroutine...")
+		close(done)
+	}()
+
+	<-terminated
+	fmt.Println("Done")
+}
+
+/** output
+canceling doWork goroutine...
+doWork exited.
+Done
+ */
