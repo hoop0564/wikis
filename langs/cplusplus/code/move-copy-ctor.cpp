9eed4b55c78f414c50f70bd5ecd3d3fb32394900
guanzhongcai <zhongcai.guan@united-imaging.com>
2021-05-07 21:30:18 +0800

    ## 移动语义 ## 完美转发 ## 移动拷贝

--- langs/cplusplus/code/move-copy-ctor.cpp
+++ langs/cplusplus/code/move-copy-ctor.cpp
@@ -84,6 +84,13 @@ int main()
     cout << "MCtor = " << MyString::MCtor << endl;
     cout << "CAsgn = " << MyString::CAsgn << endl;
     cout << "MAsgn = " << MyString::MAsgn << endl;
+
+    vector<MyString> vecStr2;
+    vecStr2.reserve(1000); //先分配好1000个空间
+    for(int i=0;i<1000;i++){
+        MyString tmp("hello");
+        vecStr2.push_back(std::move(tmp)); //调用的是移动构造函数
+    }
 }
 
 /* 结果
