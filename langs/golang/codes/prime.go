0689e7ddaae42aa09ea4fe42af3cd3454d4c102c
guanzhongcai <zhongcai.guan@united-imaging.com>
2021-05-26 08:43:17 +0800

    prime analysis

--- langs/golang/codes/prime.go
+++ langs/golang/codes/prime.go
@@ -7,32 +7,50 @@ package main
 
 import "fmt"
 
-func Processor(seq chan int, wait chan struct{}) {
-	go func() {
+func Processor(seq chan int, wait chan struct{}, id int) {
+	id++
+	go func(idx int) {
 		prime, ok := <-seq
 		if !ok {
 			close(wait)
 			return
 		}
-		fmt.Println("prime is: ", prime)
+		fmt.Printf("协程: %d, 素数是: %d\n", idx, prime)
 		out := make(chan int)
-		Processor(out, wait)		
+		Processor(out, wait, idx)		
 		for num := range seq {
-			fmt.Println("num is: ", num)
 			if num % prime != 0 {
+				fmt.Printf("协程: %d, 标记的数是: %d\n", idx, num)
 				out <- num
 			}				
 		}
 		close(out)
-	}()
+	}(id)
 }
 
 func main() {
 	origin, wait := make(chan int), make(chan struct{})
-	Processor(origin, wait)
+	id := 0 
+	Processor(origin, wait, id)
+	// 从2开始每找到一个素数就标记所有能被该素数整除的所有数
 	for num := 2; num < 10; num++ {
 		origin <- num
 	}
 	close(origin)
 	<-wait
 }
+
+/*
+output:
+协程: 1, 素数是: 2
+协程: 1, 标记的数是: 3
+协程: 1, 标记的数是: 5
+协程: 1, 标记的数是: 7
+协程: 1, 标记的数是: 9
+协程: 2, 素数是: 3
+协程: 2, 标记的数是: 5
+协程: 2, 标记的数是: 7
+协程: 3, 素数是: 5
+协程: 3, 标记的数是: 7
+协程: 4, 素数是: 7
+*/
