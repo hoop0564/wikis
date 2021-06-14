f421be1e6faa39b30d7fac7db4c9a93121481a4b
guanzhongcai <zhongcai.guan@united-imaging.com>
2021-06-15 07:14:58 +0800

    Reader & Writer interface for log process

--- langs/golang/go-concurrency-book/cmd/log-process/main.go
+++ langs/golang/go-concurrency-book/cmd/log-process/main.go
@@ -8,18 +8,29 @@ import (
 
 // 从文件夹读取日志，分析后，存入influxDB
 
-type LogProcess struct {
-	rc          chan string // 读取chan
-	wc          chan string // 写入chan
-	path        string      // 读取文件的路径
-	influxDBDsn string
+type Reader interface {
+	Read(rc chan string)
 }
 
-// ReadFromFile 读取模块
-func (l *LogProcess) ReadFromFile() {
+type Writer interface {
+	Write(wc chan string)
+}
 
+type LogProcess struct {
+	rc    chan string // 读取chan
+	wc    chan string // 写入chan
+	read  Reader
+	write Writer
+}
+
+type ReadFromFile struct {
+	path string // 读取文件的路径
+}
+
+func (r *ReadFromFile) Read(rc chan string) {
+	// 读取模块
 	line := "message"
-	l.rc <- line
+	rc <- line
 }
 
 // Process 解析模块
@@ -29,24 +40,34 @@ func (l *LogProcess) Process() {
 }
 
 // WriteToInfluxDB 写入模块
-func (l *LogProcess) WriteToInfluxDB() {
-	fmt.Println(<-l.wc)
+type WriteToInfluxDB struct {
+	influxDBDsn string // info data source
+}
+
+func (w *WriteToInfluxDB) Write(wc chan string) {
+	fmt.Println(<-wc)
 }
 
 func main() {
 
+	r := &ReadFromFile{
+		path: "/tmp/access.log",
+	}
+	w := &WriteToInfluxDB{
+		influxDBDsn: "username&password&host:port",
+	}
 	lp := &LogProcess{
-		rc: make(chan string),
-		wc: make(chan string),
-		path:        "/tmp/access.log",
-		influxDBDsn: "username&password@host:port",
+		rc:    make(chan string),
+		wc:    make(chan string),
+		read:  r,
+		write: w,
 	}
 
 	// 此写法也可以！但golang考虑到可读性，用下面的写法也OK
 	// go (*lp).ReadFromFile()
-	go lp.ReadFromFile()
+	go lp.read.Read(lp.rc)
 	go lp.Process()
-	go lp.WriteToInfluxDB()
+	go lp.write.Write(lp.wc)
 
 	time.Sleep(1 * time.Second)
 	fmt.Println("exit")
